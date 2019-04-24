fbc2e95ac292db2a2efd729da9f605bb4c5d62d6
Xurxe <xurxe.garcia@integrify.io>
2019-04-25 02:33:26 +0300

    Set up project

---
+++ gatsby-config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    pathPrefix: "/Integrify-17-clone-react",
+}
---
+++ src/components/layout.js
@@ -0,0 +1,19 @@
+import React from "react"
+import layoutStyles from "./layout.css"
+
+
+export default ({children}) => (
+    <div>
+        <header>
+
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+            
+        </footer>
+    </div>
+)
