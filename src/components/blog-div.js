ffa5c3f203790782d12ac24946dc63b07a61f802
Xurxe <xurxe.garcia@integrify.io>
2019-04-25 16:57:24 +0300

    Improved blog-div component

--- src/components/blog-div.js
+++ src/components/blog-div.js
@@ -3,7 +3,13 @@ import React from "react"
 import Styles from "./blog-div.css"
 
 export default ({children}) => (
-    <div className="blog">
-
+<div className="blog-div">
+    <div className="blog-thumbnail-div"></div>
+    <div className="blog-snippet-div">
+        <h5>Blog Title</h5>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, iusto dicta exercitationem laudantium quibusdam...
+        </p>
     </div>
+</div>
 )
