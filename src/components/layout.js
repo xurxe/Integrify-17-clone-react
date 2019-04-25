996c0f80eb2641363b03e75fe1004b0f48223a21
Xurxe <xurxe.garcia@integrify.io>
2019-04-25 13:02:43 +0300

    Finished layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,20 @@
 import React from "react"
 import layoutStyles from "./layout.css"
 
-
 export default ({children}) => (
-    <div>
-        <header>
+    <div id="layout">
+        <div className="grey-gradient-background"></div>
+        <div className="grey-dark-background"></div>
+        <div className="grey-darker-background"></div>
 
-        </header>
+        <header></header>
+        <div id="top"></div>
+        <div className="stripe-background"></div>
 
-        <main>
+        <main></main>
 
-        </main>
-
-        <footer>
-            
-        </footer>
+        <div id="bottom"></div>
+        <div id="info"></div>
+        <footer></footer>
     </div>
 )
